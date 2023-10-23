1815eab1263d798421da38f470341aa9f60d11a2
zh_harry <zh_harry@163.com>
2023-10-23 23:45:05 +0800

    api 接口定义

--- src/main/java/com/sparrow/boot/ApplicationBoot.java
+++ src/main/java/com/sparrow/boot/ApplicationBoot.java
@@ -5,18 +5,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 @SpringBootApplication(scanBasePackages = "com.sparrow.*")
 public class ApplicationBoot {
     private static Logger log = LoggerFactory.getLogger(ApplicationBoot.class);
-
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(ApplicationBoot.class);
 
-        springApplication.addListeners(new ApplicationListener<ContextClosedEvent>() {
+        springApplication.addListeners(new ApplicationListener<ApplicationStartingEvent>() {
+            @Override
+            public void onApplicationEvent(ApplicationStartingEvent event) {
+                System.out.printf("prepare before bean init");
+            }
+        });
 
+        springApplication.addListeners(new ApplicationListener<ContextRefreshedEvent>() {
+            @Override
+            public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+                log.info("application startup at {}", contextRefreshedEvent.getTimestamp());
+            }
+        });
+
+        springApplication.addListeners(new ApplicationListener<ContextClosedEvent>() {
             @Override
             public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
                 log.info("application closed at at {}", contextClosedEvent.getTimestamp());
