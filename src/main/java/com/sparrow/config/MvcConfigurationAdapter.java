c6fc948b8a22fc10e871b2af47bf4d34ef338ce2
张立志 <zhanglizhi@tedu.cn>
2023-11-16 11:04:07 +0800

    生成mybatis 配置文件

--- src/main/java/com/sparrow/config/MvcConfigurationAdapter.java
+++ src/main/java/com/sparrow/config/MvcConfigurationAdapter.java
@@ -1,33 +1,18 @@
 package com.sparrow.config;
 
-import com.sparrow.spring.starter.SpringServletContainer;
-import com.sparrow.spring.starter.filter.ClientInformationFilter;
-import com.sparrow.support.IpSupport;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.inject.Inject;
-import java.util.List;
-
-public class MvcConfigurationAdapter extends WebMvcConfigurationSupport {
+@Configuration
+public class MvcConfigurationAdapter implements WebMvcConfigurer {
 
 
-    @Value("${mock_login_user}")
-    private Boolean mockUser;
-
-    @Inject
-    private IpSupport ipSupport;
-
-    @Value("${authenticator.white.list}")
-    private List<String> whiteList;
-
-    @Inject
-    private SpringServletContainer springServletContainer;
-
-
-    @Bean
-    public ClientInformationFilter clientInformationFilter() {
-        return new ClientInformationFilter(-98, this.springServletContainer);
-    }
+//    @Value("${mock_login_user}")
+//    private Boolean mockUser;
+//
+//    @Inject
+//    private IpSupport ipSupport;
+//
+//    @Value("${authenticator.white.list}")
+//    private List<String> whiteList;
 }
