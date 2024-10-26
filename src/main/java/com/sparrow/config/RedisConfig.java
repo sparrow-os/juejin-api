0b9ce7f3339cbc9ef2dfde57968fafe6f268cb8c
张立志 <zhanglizhi@tedu.cn>
2024-10-26 17:51:25 +0800

    生成mybatis 配置文件

--- src/main/java/com/sparrow/config/RedisConfig.java
+++ src/main/java/com/sparrow/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.sparrow.config;
 
+import com.sparrow.spring.starter.redis.OperateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -20,34 +21,39 @@ public class RedisConfig {
      * this.redisTemplate.opsForSet()//提供了操作Set类型的所有方法
      * this.redisTemplate.opsForHash()//提供了操作Hash类型的所有方法
      * this.redisTemplate.opsForZSet()//提供了操作ZSet类型的所有方法
+     * <p>
+     * //     * @param lettuceConnectionFactory
      *
-     * @param lettuceConnectionFactory
      * @return
      */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
-        //设置key序列化方式String
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        //设置value的序列化方式json，使用GenericJackson2JsonRedisSerializer替换默认的序列化
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
-
-    @Bean
-    public RedisTemplate originRedisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
-        RedisTemplate redisTemplate = new RedisTemplate();
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
-
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+//        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+//        //设置key序列化方式String
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        //设置value的序列化方式json，使用GenericJackson2JsonRedisSerializer替换默认的序列化
+//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        redisTemplate.afterPropertiesSet();
+//        return redisTemplate;
+//    }
+//
+//    @Bean
+//    public RedisTemplate originRedisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+//        RedisTemplate redisTemplate = new RedisTemplate();
+//        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        return redisTemplate;
+//    }
     @Bean
     public StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory redisConnectionFactory) {
         return new StringRedisTemplate(redisConnectionFactory);
+    }
+
+    @Bean
+    public OperateLimiter operateLimiter(StringRedisTemplate redisTemplate) {
+        return new OperateLimiter(redisTemplate);
     }
 }
